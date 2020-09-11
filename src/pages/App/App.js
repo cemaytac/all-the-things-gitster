2dba2c5da68e14632f2afdf2207e3275fee5d7c1
Jam Wheat <jamonwheat@gmail.com>
2020-09-11 11:05:19 -0400

    remove data

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -28,50 +28,7 @@ class App extends Component {
       },
 
     ],
-    davidsThings: [
-      {
-        name: "waffle",
-        image: "https://i.imgur.com/C3PmdhO.jpg",  
-        attributes: ["tasty", "fluffy", "breakfast", "yummy"],
-      },
-      {
-        name: "giraffe",
-        image: "https://i.imgur.com/qIIVePm.jpg",  
-        attributes: ["tall", "cool", "fun", "animal", "(David doesn't acually own a giraffe)"], 
-      },
-      {
-        name: "Rugby",
-        image: "https://i.imgur.com/X4VC4nm.jpg",  
-        attributes: ["scruffy", "happy", "sleepy", "wise"], 
-      },
-      {
-        name: "Poptart®",
-        image: "https://i.imgur.com/q9zgcaP.jpg",  
-        attributes: ["pastry?", "food?", "edible?", "Horrible when toasted!"], 
-      },
-    ],
-    shahzadsThings: [
-      {
-        name: "functional programming",
-        image: "https://imgs.xkcd.com/comics/functional.png",  
-        attributes: ["efficient", "reusability", "not a taco", "beautiful"],
-      },
-      {
-        name: "React hooks",
-        image: "https://images.unsplash.com/photo-1518644730709-0835105d9daa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-        attributes: ["useEffect", "useState", "not a taco", "state's best friend"], 
-      },
-      {
-        name: "BluBlockers",
-        image: "https://susanmaryetta.files.wordpress.com/2011/06/blublocker-the-hangover.png?w=584",
-        attributes: ["very attractive", "useful probably!", "good for viewing tacos", "such hip", "worn by all the cool kids"],
-      },
-      { 
-        name: "Healthy food", 
-        image: "https://i.imgur.com/gRxOxsA.jpg", 
-        attributes: ["makes you live a long time", "consistent poops", "probably tacos sometimes!", "farm to market"], 
-      },
-    ] 
+  
   } 
   render() {
     return (
